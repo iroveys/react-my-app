9b0fb9aaf125b70b6db6879481ae2bd727b6b93d
iroveys <iroveys@gmail.com>
2024-06-03 20:55:10 +0330

    matin 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = React.createElement(
-  "div",
-  { id: "emad", className: "emadclass" },
-  "Hello world"
-);
+// THIRD WAY
+function App() {
+  return (
+    <div id="emad" className="emadclass">
+      Hello Matin3
+    </div>
+  );
+}
+
+// SECOND WAY
+// const element = (
+//   <div id="emad" className="emadclass">
+//     Hello Ali2
+//   </div>
+// );
+
+//FIRST WAY
+// const element = React.createElement(
+//   "div",
+//   { id: "emad", className: "emadclass" },
+//   "Hello world"
+// );
 
 const root = document.getElementById("root");
 
-ReactDOM.render(element, root);
+// ReactDOM.render(element, root);
+ReactDOM.render(<App />, root);
